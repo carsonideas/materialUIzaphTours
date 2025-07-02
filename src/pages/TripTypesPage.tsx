ff4c64c60d49deb5cf6029ed5d9e0811eff9e1ea
Ideahub <douglascarson1121@gmail.com>
2025-07-02 12:48:01 +0300

    trip type grid issue

--- src/pages/TripTypesPage.tsx
+++ src/pages/TripTypesPage.tsx
@@ -142,7 +142,7 @@ const TripTypesPage = () => (
           gridTemplateColumns: {
             xs: "1fr",
             sm: "repeat(2, 1fr)",
-            md: "repeat(3, 1fr)",
+            md: "repeat(2, 1fr)",
           },
           gap: 4,
         }}
